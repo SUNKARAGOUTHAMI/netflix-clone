7d9286423dcc1b0677153eb662d4e57b04877cf2
gouthami <131252737+SUNKARAGOUTHAMI@users.noreply.github.com>
2025-11-15 04:52:02 +0530

    Update movie.ts

--- types/movie.ts
+++ types/movie.ts
@@ -24,12 +24,10 @@ export interface MovieSummary {
   id: number;
   title: string;
   overview: string;
-    poster_path: string;
-  release_date: string;
-  vote_average: number;
-
   backdrop_path: string;
-  // add any other relevant fields
+  poster_path?: string;
+  release_date?: string;
+  vote_average?: number;
 }
 
 export interface MovieDetail extends MovieSummary {
